76304245620dc65b354bbe00523f663dd08ae0bd
Patrick Roche <patrick.l.roche@gmail.com>
2015-09-08 20:02:42 -0400

    add my changes through Lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,109 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var work = {
+	"jobs" : [
+		{
+			"employer" : "Job 1 Employer Name",
+			"title" : "Job 1 Title",
+			"location" : "Job 1 Location",
+			"dates" : "Job 1 start date - Job 1 end date",
+			"description" : "Job 1 Description" 	
+		},
+		{
+			"employer" : "Job 2 Employer Name",
+			"title" : "Job 2 Title",
+			"location" : "Job 2 Location",
+			"dates" : "Job 2 start date - Job 2 end date",
+			"description" : "Job 2 Description" 	
+		}
+	]
+};
+
+var projects = {
+	"projects" : [
+		{	"title" : "Project 1 Title", 
+			"dates" : "Project 1 start date - Project 1 end date",
+			"description" : "Project 1 Description",
+			"images" : [
+				{"image1" : "image1URL"},
+				{"image2" : "image2URL"}
+			]
+		},
+		{	"title" : "Project 1 Title", 
+			"dates" : "Project 1 start date - Project 1 end date",
+			"description" : "Project 1 Description",
+			"images" : [
+				{"image1" : "image1URL"},
+				{"image2" : "image2URL"}
+			]
+		}
+	]
+}
+
+var bio = {
+	"name" : "Patrick Roche",
+	"role" : "Web Developer",
+	"welcomeMessage" : "Welcome to my resume!",
+	
+	"contacts" : {
+		"mobile" : "724-875-3487",
+		"email" : "patrick.l.roche@gmail.com",
+		"github" : "plr108",
+		"location" : "Pittsburgh"
+	},
+	
+	"skills" : [
+		"HTML","CSS","JavaScript","hustle","loyalty","respect","grit","awesomeness"
+	],
+	
+	"bioPic" : "images/me.jpg"
+};
+
+var education = {
+	"schools" : [
+		{"school" : {
+			"name" : "school1", 
+			"city" : "school1location",
+			"degree" : "school1degree",
+			"majors" : [
+				"major1"
+				],
+			"dates" : "start date - end date",
+			"url" : "school1url"
+			}
+		},
+		{"school" : {
+			"name" : "school1", 
+			"city" : "school1location",
+			"degree" : "school1degree",
+			"majors" : [
+				"major1"
+				],
+			"dates" : "start date - end date",
+			"url" : "school1url"
+			}
+		}
+	],
+	"onlineCourses" : [
+		{"onlineCourse" : {
+			"title" : "onlineCourse1 title",
+			"school" : "onlineCourse1 school",
+			"dates" : "start date - end date",
+			"url" : "courseURL"
+			} 
+		}
+	]
+}
+
+if(bio.skills.length > 0) {
+	console.log(bio.skills.length);
+	$("#header").append("MadSkillz");
+
+	var formattedSkill = HTMLskills.replace("%date%", "Skill1");
+
+	$("#skills").append(formattedSkill);
+
+}
+else
+{
+	console.log("you have no skills");
+}
+
