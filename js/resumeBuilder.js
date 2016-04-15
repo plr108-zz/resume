3b7a7069b986b96ee5a3f0ad9362d51c93ed521d
Patrick Roche <patrick.l.roche@gmail.com>
2016-04-15 18:16:32 -0400

    Change current job title

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -320,7 +320,7 @@ var education = {
 var work = {
     "jobs": [{
         "employer": "Philips Healthcare",
-        "title": "Process Control Engineer",
+        "title": "Senior Engineering Specialist",
         "location": "Pittsburgh, PA",
         "dates": "2010 - Present",
         "description": "Lead the development of processes to remanufacture medical devices at over 55 Philips and external service centers.  Create validation strategies for 24 product lines resulting in zero major findings in internal and external audits.  Continually improving the procedures related to process validation, including reducing equipment qualification times by 30%.",
